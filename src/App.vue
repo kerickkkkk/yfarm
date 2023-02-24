dc6bfa33f7cd6da46a2a02df08b99745d8df3317
eric <howcrazys@gmail.com>
2023-02-24 13:57:50 +0800

    feat: 初步套件安裝完成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
+<script setup>
+
+</script>
+
 <template>
   <router-view />
 </template>
-
-<script>
-export default {}
-</script>
 
 <style lang="scss">
 
--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import pinia from './stores/index'
+import '@/scss/all.scss'
+import 'bootstrap'
+import VueSweetAlert from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 const app = createApp(App)
 
+app.use(pinia)
 app.use(router)
+app.use(VueSweetAlert)
 app.mount('#app')

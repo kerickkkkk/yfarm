79a6d0977168a4b9b6a2297a9a1d2122ff9d9be9
eric <howcrazys@gmail.com>
2023-03-14 21:05:32 +0800

    feat: 建議區塊

---
+++ src/components/swiper/SuggestionSwiper.vue
@@ -0,0 +1,58 @@
+<script setup>
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { ref, onMounted } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Pagination } from 'swiper'
+
+import ProductCard from '@/components/ProductCard.vue'
+
+const props = defineProps({
+  items: {
+    type: Array,
+    default () {
+      return []
+    }
+  }
+})
+
+const modules = ref(
+  [Pagination]
+)
+onMounted(() => {
+
+})
+</script>
+<template>
+  <Swiper
+    v-if="items.length > 0"
+    :slides-per-view="1"
+    :space-between="30"
+    :pagination="true"
+    :modules="modules"
+    :breakpoints="{
+      '768': {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    }"
+    class="container position-relative rounded-5 rounded-top-0"
+  >
+    <SwiperSlide
+      v-for="item in props.items"
+      :key="item"
+    >
+      <div class="w-100">
+        <ProductCard
+          :item="item"
+        />
+      </div>
+    </SwiperSlide>
+  </Swiper>
+  <div v-else>
+    沒有資料
+  </div>
+</template>
+<style lang="scss">
+
+</style>

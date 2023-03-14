db0b659d1c5768a4729c328c7e4c4157cb4310ae
eric <howcrazys@gmail.com>
2023-03-14 08:58:40 +0800

    feat: temp image 封裝

---
+++ src/components/base/ImageComponent.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+const props = defineProps({
+  imageUrl: {
+    type: String,
+    default () {
+      return ''
+    }
+  },
+  alt: {
+    type: String,
+    default () {
+      return ''
+    }
+  }
+})
+const tempUrl = ref('')
+const onImageError = () => {
+  tempUrl.value = '/logo.png'
+}
+const onImageLoad = () => {
+  console.log('loadinng')
+}
+onMounted(() => {
+
+})
+</script>
+
+<template>
+  <img
+    :src="tempUrl"
+    :alt="props.alt"
+    @error="onImageError"
+    @load="onImageLoad"
+  >
+</template>
+
+<style scoped lang="scss">
+
+</style>

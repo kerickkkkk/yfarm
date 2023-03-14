bb0fdef798af6c7a9937b3e42bf41cde24083bb5
eric <howcrazys@gmail.com>
2023-03-14 20:56:33 +0800

    feat: 選擇數量元件

---
+++ src/components/base/SelectCountComponent.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from 'vue'
+const emit = defineEmits(['setQty'])
+const props = defineProps({
+  qty: {
+    type: Number,
+    default () {
+      return 1
+    }
+  },
+  maxQty: {
+    type: Number,
+    default () {
+      return 10
+    }
+  }
+})
+const tempQty = ref(props.qty)
+</script>
+
+<template>
+  <div class="input-group mb-0 w-50 mx-auto">
+    <button
+      id="button-addon1"
+      class="btn btn-outline-neutral"
+      type="button"
+      :disabled="tempQty <= 1"
+      @click="emit('setQty', --tempQty)"
+    >
+      —
+    </button>
+    <select
+      v-model="tempQty"
+      class="form-control"
+    >
+      <option
+        v-for="n in props.maxQty"
+        :key="n"
+        :value="n"
+        class="text-center"
+      >
+        {{ n }}
+      </option>
+    </select>
+    <button
+      class="btn btn-outline-neutral"
+      type="button"
+      :disabled="tempQty >= maxQty"
+      @click="emit('setQty', ++tempQty)"
+    >
+      <i class="bi bi-plus" />
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>

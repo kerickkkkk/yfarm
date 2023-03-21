42bfc4029fa8d220f33276d8424ce09d8e63d720
eric <howcrazys@gmail.com>
2023-03-22 01:03:49 +0800

    deploy

---
+++ assets/BannerView-20631040.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vueexport-helper-c27b6911.js";import{o,c}from"./index-fa012ce5.js";const r={},t={class:"bg-cover mb-12",style:{height:"40vh",backgroundImage:"url(./images/ImgAbout01.png)"}};function n(s,a){return o(),c("div",t)}const i=e(r,[["render",n]]);export{i as B};
--- assets/OriginalIntention-18eb024a.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./plugin-vueexport-helper-c27b6911.js";import{o as t,c as o,F as c,d as e}from"./index-cc8df20e.js";const r={},s=e("h1",null,"創立初衷",-1),a=e("p",null," 從小就在田野中長大的鄕下孩子，扣除上學的時間外，孩提的記憶中都是在田野或是三合院度過，手上拿著東西不是鉛筆或是GAME BOY，而是鐮刀跟鋤頭，眼裡看到的景色就是黃黃的一堆土，藍藍的一片天，綠綠的一畝田。 ",-1);function _(l,i){return t(),o(c,null,[s,a],64)}const m=n(r,[["render",_]]);export{m as default};

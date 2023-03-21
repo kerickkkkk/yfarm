6599c8d2b9e29c642dc43c36c340794c201dd5f9
eric <howcrazys@gmail.com>
2023-03-22 00:40:54 +0800

    deploy

--- assets/BannerView-fe3a556a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./plugin-vueexport-helper-c27b6911.js";import{o,c}from"./index-8b0852ca.js";const r={},t={class:"bg-cover mb-12",style:{height:"40vh",backgroundImage:"url(./images/ImgAbout01.png)"}};function n(s,a){return o(),c("div",t)}const i=e(r,[["render",n]]);export{i as B};
--- assets/articles-5e448c31.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./service-ed2548d5.js";function i(e=1){return r({method:"GET",url:`/articles?page=${e}`})}function a(e){return r({method:"GET",url:`/article/${e}`})}export{a,i as g};

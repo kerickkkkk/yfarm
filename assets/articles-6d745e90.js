7b8ff7a646bd1ccb22240def91fe4f33c30a3f58
eric <howcrazys@gmail.com>
2023-03-22 00:52:10 +0800

    deploy

---
+++ assets/articles-6d745e90.js
@@ -0,0 +1,1 @@
+import{s as r}from"./service-bb52b294.js";function i(e=1){return r({method:"GET",url:`/articles?page=${e}`})}function a(e){return r({method:"GET",url:`/article/${e}`})}export{a,i as g};

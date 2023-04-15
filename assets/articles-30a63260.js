c548ef1590730ea66fbde48ed3b856da2f4375c1
eric <howcrazys@gmail.com>
2023-04-15 11:13:36 +0800

    deploy

---
+++ assets/articles-30a63260.js
@@ -0,0 +1,1 @@
+import{s as r}from"./service-97f28bed.js";function i(e=1){return r({method:"GET",url:`/articles?page=${e}`})}function a(e){return r({method:"GET",url:`/article/${e}`})}export{a,i as g};
--- assets/articles-445cb23b.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./service-76d60509.js";function i(e=1){return r({method:"GET",url:`/articles?page=${e}`})}function a(e){return r({method:"GET",url:`/article/${e}`})}export{a,i as g};
